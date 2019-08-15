fbaa4812921fda31f30dc471e2c5cba1811e5e7b
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-15 21:56:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+  return
+  string.toUpperCase()
+}
+
+function whisper(string){
+  return
+  string.toLowerCase()
+}
+
+function logShout(string){
+  return
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  return
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === string){
+    return "I can't Hear you!"
+  }
+  if (string.toUpperCase() === sting){
+    return "Yes indeed!"
+  }
+}

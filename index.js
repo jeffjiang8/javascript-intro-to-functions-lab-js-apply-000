e5bbd97f0fa9511bbec3d1c1ed72e14b4afb4ba0
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-15 21:56:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
-    return "I can't Hear you!"
+    return "I can't hear you!"
   }
   if (string.toUpperCase() === sting){
     return "Yes indeed!"
